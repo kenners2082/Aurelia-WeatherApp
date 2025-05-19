66ec2a58f65b3a1a7db63f2748e6fc9e15a7251d
McKenna Todd <mtodd2082@gmail.com>
2025-05-19 15:16:25 +0200

    Update weather-service.ts - remove sensitive info

--- weather-app/src/services/weather-service.ts
+++ weather-app/src/services/weather-service.ts
@@ -3,8 +3,7 @@ import { HttpClient } from '@aurelia/fetch-client';
 export class WeatherService {
 
     private http: HttpClient = new HttpClient();
-    //private apiKey = '8e737b9385a8e4071a989a3aee956e85';
-    //private baseUrl = 'https://api.openweathermap.org/data/2.5/';
+
 
     async getWeatherByCity(city: string): Promise<any> {
         const response = await this.http.fetch('http://localhost:3001/api/data');
